d79c63e8c7034dd12af92269f542f32c7cbca484
dipneupane <neupanedip3@gmail.com>
2022-11-14 16:37:19 +0545

    Text changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hay Dipendra. Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
